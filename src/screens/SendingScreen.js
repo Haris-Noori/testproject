2f817954ebd678d36782279441924da71350c966
M.Haris Noori <harisnoori20@gmail.com>
2023-06-13 18:29:22 +0100

    animations to add in SendingScreen

--- src/screens/SendingScreen.js
+++ src/screens/SendingScreen.js
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
+import { scale } from 'react-native-size-matters';
+import Toast from 'react-native-simple-toast';
 
 const SendingScreen = () => {
   return (
-    <View>
-      <Text>SendingScreen</Text>
+    <View style={styles.parent}>
+      <View style={styles.topContainer}>
+        <Text style={styles.heading}>Sending Money...</Text>
+        <Text style={styles.description}>Ensure that the two mobile devices are within 5 cm and both have NFC connectivity.</Text>
+      </View>
+      <View>
+        <Text>Animations here</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.cancelButton}>
+          <Text>Cancel</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: scale(16),
+  },
+  topContainer: {
+    alignItems: 'center',
+    marginTop: scale(20),
+  },
+  heading: {
+    fontSize: scale(20),
+    fontWeight: '400',
+    color: '#000000',
+  },
+  description: {
+    textAlign: 'center',
+    fontSize: scale(10),
+    fontWeight: '400',
+    color: '#838383'
+  },
+  buttonContainer: {
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    marginBottom: scale(20),
+    width: '100%'
+  },
+  cancelButton: {
+    borderWidth: 1,
+    borderColor: '#A010A3',
+    height: scale(44),
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: scale(25),
+  }
+});
 
 export default SendingScreen;
