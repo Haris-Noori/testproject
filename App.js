ef0b720b82ba4d4e515e88f1d2e1eecb06a07b5c
M.Haris Noori <harisnoori20@gmail.com>
2023-06-13 14:07:04 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView, View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={Styles.parent}>
+      <View>
+        <Text>Pyng Mobile App by Haris</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const Styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+  },
+});
+
+export default App;
